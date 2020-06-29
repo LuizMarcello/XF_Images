86a4155867480d11f05463b602a0dfa57c7189b2
luizmarcelloxamarin <luiztelemicro@hotmail.com>
2020-06-29 01:49:07 -0300

    29.06.2020_01:48hs

---
+++ XF_Images/XF_Images/XF_Images/ImagePage3.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF_Images
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ImagePage3 : ContentPage
+    {
+        public ImagePage3()
+        {
+            InitializeComponent();
+        }
+    }
+}
