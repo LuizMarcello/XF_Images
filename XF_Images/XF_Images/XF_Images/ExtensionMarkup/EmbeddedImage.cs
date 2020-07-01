6290d7ac72fdccf130ebe2a414a6056b2c1e38bb
luizmarcelloxamarin <luiztelemicro@hotmail.com>
2020-06-30 21:29:24 -0300

    30.06.2020_21:29

--- XF_Images/XF_Images/XF_Images/ExtensionMarkup/EmbeddedImage.cs
+++ XF_Images/XF_Images/XF_Images/ExtensionMarkup/EmbeddedImage.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
 
 namespace XF_Images.ExtensionMarkup
 {
     public class EmbeddedImage : IMarkupExtension
     {
+        public string ResourceId { get; set; }
         public object ProvideValue(IServiceProvider serviceProvider)
         {
-            public string ResourceId { get; set; }
+            if (string.IsNullOrWhiteSpace(ResourceId))
+                return null;
 
-            
+            return ImageSource.FromResource(ResourceId);
         }
     }
 }
+            
+
+            
+
