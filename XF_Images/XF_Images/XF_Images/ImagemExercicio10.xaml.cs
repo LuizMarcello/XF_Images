0feae3c009690124bdfc066dfcd07a9916d971c7
luizmarcelloxamarin <luiztelemicro@hotmail.com>
2020-07-06 23:00:15 -0300

    06.07.2020_22:59hs

--- XF_Images/XF_Images/XF_Images/ImagemExercicio10.xaml.cs
+++ XF_Images/XF_Images/XF_Images/ImagemExercicio10.xaml.cs
@@ -12,11 +12,47 @@ namespace XF_Images
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ImagemExercicio10 : ContentPage
     {
+        private int _IdImagemAtual = 1;
+
         public ImagemExercicio10()
         {
             InitializeComponent();
+
+            _IdImagemAtual = 1;
+            CarregarImagem();
+        }
+
+        void CarregarImagem()
+        {
+            imagge.Source = new UriImageSource
+            {
+                Uri = new Uri(String.Format("http://macoratti.net/Imagens/animais/cao{0}.jpg", _IdImagemAtual)),
+                CachingEnabled = false
+            };
+        }
+        private void Anterior_Clicked(object sender, EventArgs e)
+        {
+            _IdImagemAtual--;
+            //if (_IdImagemAtual == 0)
+            //    _IdImagemAtual = 10;
+
+            CarregarImagem();
+        }
+
+        private void Proximo_Clicked(object sender, EventArgs e)
+        {
+            _IdImagemAtual++;
+            //if (_IdImagemAtual == 11)
+            //    _IdImagemAtual = 1;
+
+            CarregarImagem();
         }
     }
 }
+
+
+
+
+
 
 
